1eeb61ea30328ed2a55b974946626bfa5578ad46
SamuelKelechi <kcfreshie@ymail.com>
2023-06-07 13:47:37 +0100

    Added Post Method

--- src/App.js
+++ src/App.js
@@ -5,84 +5,39 @@ import {FaBars, FaTimes} from "react-icons/fa"
 function App() {
 
   const [click, setclick] = useState(false)
+  const [names, setNames] = useState("")
+  const [sexs, setSexs] = useState("")
+  const [courses, setCourses] = useState("")
+  const [ages, setAges] = useState("")
 
-  const [state, setState] = useState([
-    {
-      id: 1,
-      name: 'chelsea',
-      sex: 'male',
-      course: 'web development',
-      age: 18,
-    },
-    {
-      id: 2,
-      name: 'precious',
-      sex: 'male',
-      course: 'mobile development',
-      age: 28,
-    },
-    {
-      id: 3,
-      name: "Rachael",
-      sex: 'female',
-      course: "cloud engineer",
-      age: 25,
-    },
-    {
-      id: 4,
-      name: "Samuel",
-      sex: 'Female',
-      course: 'UI/UX',
-      age: 25,
-    },
-    {
-      id: 5,
-      name: "Faith",
-      sex: 'Female',
-      course: 'mobile development',
-      age: 25,
-    },
-    {
-      id: 6,
-      name: "Cynthia",
-      sex: 'Female',
-      course: 'Back-End Engineer',
-      age: 25,
-    },
-    {
-      id: 7,
-      name: "Ayo",
-      sex: 'Male',
-      course: 'mobile development',
-      age: 25,
-    },
-    {
-      id: 8,
-      name: "Faith",
-      sex: 'Female',
-      course: 'mobile development',
-      age: 25,
-    },
-    {
-      id: 9,
-      name: "Femi",
-      sex: 'Male',
-      course: 'mobile development',
-      age: 25,
-    },
-    {
-      id: 10,
-      name: "Hope",
-      sex: 'Female',
-      course: 'Full Stack Engineer',
-      age: 25,
-    }
-  ])
+  const [state, setState] = useState([])
 
 
   const handClick = () => {
     setclick (!click)
   }
+
+  console.log(names)
+  console.log(sexs)
+  console.log(courses)
+  console.log(ages)
+
+  const Post = () => {
+    const Items = {
+      id : state.length + 1,
+      names : names,
+      sex : sexs,
+      course : courses,
+      age : ages,
+    }
+    setState([...state, Items]);
+
+    setNames('');
+    setAges('');
+    setCourses('');
+    setSexs('');
+  }
+
 
   return (
     <div className="Container">
@@ -95,29 +50,52 @@ function App() {
             <li>SERVICES</li>
             <li>CONTACT</li>
           </navs>
+
           <div className="Icon" onClick={handClick}>
             {click ? <FaTimes/> : (<FaBars/>)}
             {/* {click ? <div>Its my turn</div> : <h2>No, No... </h2>} */}   
           </div>
         </div>
+
+
         
       </div>
+
+      
+    <div className= "App">
+      <div className="Wrap">
+        <input placeholder= "Name" value={names} onChange={(e) =>{
+          setNames(e.target.value)
+        }}/>
+        <input placeholder= "Sex" value={sexs} onChange={(e) =>{
+          setSexs(e.target.value)
+        }}/>
+        <input placeholder= "Course" value={courses} onChange={(e) =>{
+          setCourses(e.target.value)
+        }}/>
+        <input placeholder= "Age" value={ages} onChange={(e) =>{
+          setAges(e.target.value)
+        }}/>
+        <button onClick={Post}>POST</button>
+      </div>
+    </div>
       <wrapper className= "Cardwrapper">
         {state.map((el) =>{
           return(
-            <card className= "Card">
+            <div className="Card" key={el.id}>
             <span>{el.id}</span>
             <span>{el.name}</span>
             <span>{el.sex}</span>
             <span>{el.course}</span>
             <span>{el.age}</span>
-        </card>
+        </div>
           )
         })}
        
       </wrapper>
 
     </div>
+
   );
 }
 
